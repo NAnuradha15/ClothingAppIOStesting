b2e1c7ab6ac723e051021d29c3bdf3048c2f7603
Nilakshi Anuradha <98905552+NAnuradha15@users.noreply.github.com>
2024-03-24 11:06:54 +0530

    Initial Commit

---
+++ ClothingAppIOS/ClothingAppIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClothingAppIOSApp.swift
+//  ClothingAppIOS
+//
+//  Created by User on 2024-03-24.
+//
+
+import SwiftUI
+
+@main
+struct ClothingAppIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
